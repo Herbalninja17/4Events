7c60043cdcb01401cd81cf9c044e5ede1e05457c
jeetjeweet <jethrovanderkolk@gmail.com>
2016-10-17 13:27:06 +0200

    edit participant form toegevoegd

---
+++ EyeCT4Events/GUI/EditParticipantForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EyeCT4Events.GUI
+{
+    public partial class EditParticipantForm : Form
+    {
+        public EditParticipantForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Om naar de form te gaan om een account aan te maken.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreateParticipant_Click(object sender, EventArgs e)
+        {
+            CreateParticipantForm cpf = new CreateParticipantForm();
+            this.Hide();
+            cpf.Show();
+        }
+
+        /// <summary>
+        /// Om naar de form te gaan om je account te verwijderen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDeleteParticipant_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        ///  Laat de algemene participants form zien.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnParticipants_Click(object sender, EventArgs e)
+        {
+            ParticipantsForm pf = new ParticipantsForm();
+            this.Hide();
+            pf.Show();
+        }
+
+        /// <summary>
+        /// Om je account te wijzigen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEditParticipantEditParticipant_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
