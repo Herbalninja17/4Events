7c95687a3f6682f8f170fc6c035792e32346ea2b
IJpelaar,Bono B.G.M. <b.ijpelaar@student.fontys.nl>
2016-10-17 15:19:45 +0200

    File ingevuld, comments toegevoegd.

--- EyeCT4Events/Business/Classes/File.cs
+++ EyeCT4Events/Business/Classes/File.cs
@@ -8,11 +8,54 @@ namespace EyeCT4Events
 {
     public class File
     {
-        public string Title { get; set; }
-        public FileType FileType { get; set; }
-        public int FileID { get; set; }
-        public Person Poster { get; set; }
+        private string title;
+        private FileType fileType;
+        private int fileID;
+        private Person poster;
 
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    if (value == null) { throw new ArgumentNullException("title"); }
+                    throw new ArgumentException("title");
+                }
+                title = value;
+            }
+        }
+
+        public FileType FileType
+        {
+            get { return fileType; }
+            set { fileType = value; }
+        }
+
+        public int FileID
+        {
+            get { return fileID; }
+            set
+            {
+                if (value == null) { throw new ArgumentOutOfRangeException("fileID");}
+                fileID = value;
+            }
+        }
+
+        public Person Poster
+        {
+            get { return poster; }
+            set { poster = value; }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="title">Title of the file</param>
+        /// <param name="fileType">The document type of the file</param>
+        /// <param name="fileID">The ID of the file</param>
+        /// <param name="poster">The person who posted the file</param>
         public File(string title, FileType fileType, int fileID, Person poster)
         {
             Title = title;
