8a36db5a704fee457744296de0856414135a841f
IJpelaar,Bono B.G.M. <b.ijpelaar@student.fontys.nl>
2016-10-17 21:12:47 +0200

    Reservation

--- EyeCT4Events/Business/Classes/Reservation.cs
+++ EyeCT4Events/Business/Classes/Reservation.cs
@@ -8,18 +8,71 @@ namespace EyeCT4Events
 {
     public class Reservation
     {
-        public int ReservationID { get; set; }
-        public bool EventIsPlayed { get; set; }
+        private int reservationID;
+        private bool eventIsPlayed;
+        private List<Person> persons;
+        private List<Material> materials;
 
+        public int ReservationID
+        {
+            get { return reservationID; }
+            set
+            {
+                if (value == null) { throw new ArgumentOutOfRangeException("reservationID");}
+                reservationID = value;
+            }
+        }
+
+        public bool EventIsPlayed
+        {
+            get { return eventIsPlayed; }
+            set { eventIsPlayed = value; }
+        }
+
+        public List<Person> Persons
+        {
+            get { return persons; }
+            set { persons = value; }
+        }
+
+        public List<Material> Materials
+        {
+            get { return materials; }
+            set { materials = value; }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="reservationID">The ID of the reservation</param>
+        /// <param name="eventIsPlayed">If the event is playing or not</param>
         public Reservation(int reservationID, bool eventIsPlayed)
         {
             ReservationID = reservationID;
             EventIsPlayed = eventIsPlayed;
+            Persons = new List<Person>();
+            Materials = new List<Material>();
         }
 
         public decimal CalculatePrice()
         {
+            if (materials != null)
+            {
+                decimal price = 0;
+
+                foreach (Material m in materials)
+                {
+                    if (!m.IsPayed)
+                    {
+                        price += m.Price;
+                    }
+                }
+
+                return price;
+            }
+
             return 0;
+
         }
 
         public List<Material> OpenMaterialPayment()
@@ -29,16 +82,48 @@ namespace EyeCT4Events
 
         public decimal CalculateMaterialPayment()
         {
+            if (materials != null)
+            {
+                decimal price = 0;
+
+                foreach (Material m in materials)
+                {
+                    if (!m.IsPayed)
+                    {
+                        price += m.Price;
+                    }
+                }
+
+                return price;
+            }
+
             return 0;
         }
 
-        public void AddPerson(Person person)
+        public bool AddPerson(Person person)
         {
-            
+            foreach (Person p in persons)
+            {
+                if (p == person)
+                {
+                    return false;
+                }
+            }
+
+            Persons.Add(person);
+            return true;
         }
 
         public bool RemovePerson(Person person)
         {
+            foreach (Person p in persons)
+            {
+                if (p == person)
+                {
+                    persons.Remove(p);
+                    return true;
+                }
+            }
             return false;
         }
     }
