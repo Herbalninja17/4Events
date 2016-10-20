f2ad1ec4b4adc4d26bde474b1536c57e69c1858f
Timothy <timothymarkus@live.nl>
2016-10-20 12:28:53 +0200

    Aanpassingen gemaakt in: Person, File, CampingSpot klasses gemaakt.
    Unit-tests voor deze klasses uitgewerkt.

--- EyeCT4Events_Testprojects/Business_Tests/CampingSpot_Tests.cs
+++ EyeCT4Events_Testprojects/Business_Tests/CampingSpot_Tests.cs
@@ -8,8 +8,89 @@ namespace EyeCT4Events_Testprojects.Business_Tests
     public class CampingSpot_Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CampingSpot_SpotIDMayNotBeLessThan1()
         {
+            //Arrange
+            SpotType spotType = SpotType.Standard;
+            int spotID = 0;
+            int capacity = 1;
+
+            //Act
+            CampingSpot campingSpot = new CampingSpot(spotType, spotID, capacity);
+
+            //Assert is handled by the ExcpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CampingSpot_CapacityMayNotBeLessThan1()
+        {
+            //Arrange
+            SpotType spotType = SpotType.Standard;
+            int spotID = 1;
+            int capacity = 0;
+
+            //Act
+            CampingSpot campingSpot = new CampingSpot(spotType, spotID, capacity);
+
+            //Assert is handled by the ExcpectedException
+        }
+
+        [TestMethod]
+        public void CampingSpot_Constructor_AllSettingsMinimum()
+        {
+            //Arrange
+            SpotType spotType = SpotType.Standard;
+            int spotID = 1;
+            int capacity = 1;
+
+            //Act
+            CampingSpot campingSpot = new CampingSpot(spotType, spotID, capacity);
+
+            //Assert
+            Assert.IsTrue(campingSpot.SpotType == SpotType.Standard);
+            Assert.IsTrue(campingSpot.SpotID == 1);
+            Assert.IsTrue(campingSpot.Capacity == 1);
+            Assert.IsFalse(campingSpot.Reserved);
+        }
+
+        [TestMethod]
+        public void CampingSpot_ReservationCanBeSet()
+        {
+            //Arrange
+            SpotType spotType = SpotType.Standard;
+            int spotID = 1;
+            int capacity = 1;
+
+            //Act 1
+            CampingSpot campingSpot = new CampingSpot(spotType, spotID, capacity);
+
+            //Assert 1
+            Assert.IsFalse(campingSpot.Reserved);
+
+            //Act 2
+            campingSpot.Reserved = true;
+
+            //Assert 2
+            Assert.IsTrue(campingSpot.Reserved);
+        }
+
+        [TestMethod]
+        public void CampingSpot_ToString()
+        {
+            //Arrange
+            SpotType spotType = SpotType.Standard;
+            int spotID = 1243;
+            int capacity = 4;
+            CampingSpot campingSpot = new CampingSpot(spotType, spotID, capacity);
+            campingSpot.Reserved = true;
+
+            //Act 1
+            string testString = campingSpot.ToString();
+
+            //Assert 1
+            Assert.IsTrue(testString == "1243 | Standard | 4 | reserved");
         }
     }
 }
--- EyeCT4Events_Testprojects/Business_Tests/File_Tests.cs
+++ EyeCT4Events_Testprojects/Business_Tests/File_Tests.cs
@@ -7,9 +7,122 @@ namespace EyeCT4Events_Testprojects.Business_Tests
     [TestClass]
     public class File_Tests
     {
-        [TestMethod]
-        public void TestMethod1()
+        /// <summary>
+        /// Basic setup for person.
+        /// MAKING CHANGES TO THIS WILL MAKE CERTAIN TESTS FAIL!
+        /// </summary>
+        /// <returns>A person to use for testing.</returns>
+        public Person MakePerson()
         {
+            string name = "Batman";
+            DateTime birthdate = new DateTime(1984, 3, 11);
+            string address = "Wayne Manor";
+            string phoneNumber = "077 - 888 412 12 14 3";
+            string userName = "Not_Bruce_Wayne";
+            string email = "Catwomanlover69@wayneenterprices.GC";
+            string password = "I_am_Batman69";
+
+            Person person = new Person(name, birthdate, address, phoneNumber, userName, email, password);
+
+            return person;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void File_TitleMayNotBeNull()
+        {
+            //Arrange
+            string title = null;
+            FileType filetype = FileType.Video;
+            int fileID = 1;
+            Person poster = MakePerson();
+
+            //Act
+            File file = new File(title, filetype, fileID, poster);
+
+            //Assert is handled by the ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void File_TitleMayNotBeSpaces()
+        {
+            //Arrange
+            string title = "";
+            FileType filetype = FileType.Video;
+            int fileID = 1;
+            Person poster = MakePerson();
+
+            //Act
+            File file = new File(title, filetype, fileID, poster);
+
+            //Assert is handled by the ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void File_FileIDMayNotBeLessThanOne()
+        {
+            //Arrange
+            string title = "Awesome Video Mashup";
+            FileType filetype = FileType.Video;
+            int fileID = 0;
+            Person poster = MakePerson();
+
+            //Act
+            File file = new File(title, filetype, fileID, poster);
+
+            //Assert is handled by the ExpectedException
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void File_FileIDMayNotBeANegativeNumber()
+        {
+            //Arrange
+            string title = "Awesome Video Mashup";
+            FileType filetype = FileType.Video;
+            int fileID = -642;
+            Person poster = MakePerson();
+
+            //Act
+            File file = new File(title, filetype, fileID, poster);
+
+            //Assert is handled by the ExpectedException
+        }
+
+        [TestMethod]
+        public void File_Constructor()
+        {
+            //Arrange
+            string title = "Awesome Video Mashup";
+            FileType filetype = FileType.Video;
+            int fileID = 8;
+            Person poster = MakePerson();
+
+            //Act
+            File file = new File(title, filetype, fileID, poster);
+
+            //Assert
+            Assert.IsTrue(file.Title == "Awesome Video Mashup");
+            Assert.IsTrue(file.Poster.Name == "Batman");
+        }
+
+        [TestMethod]
+        public void File_ToString()
+        {
+            //Arrange
+            string title = "Awesome Video Mashup";
+            FileType filetype = FileType.Video;
+            int fileID = 8;
+            Person poster = MakePerson();
+            File file = new File(title, filetype, fileID, poster);
+
+            //Act
+            string testString = file.ToString();
+
+            //Assert
+            Assert.IsTrue(testString == "Awesome Video Mashup | Video | 8 | Not_Bruce_Wayne");
         }
     }
 }
