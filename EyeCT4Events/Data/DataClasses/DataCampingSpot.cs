50888d9ba6a1e7ca13ee628f3d29650072f47c97
IJpelaar,Bono B.G.M. <b.ijpelaar@student.fontys.nl>
2016-11-03 15:30:01 +0100

    hoi

--- EyeCT4Events/Data/DataClasses/DataCampingSpot.cs
+++ EyeCT4Events/Data/DataClasses/DataCampingSpot.cs
@@ -18,6 +18,11 @@ namespace EyeCT4Events.Data.DataClasses
             return null;
         }
 
+        public static void ReserveCampingSpot(int spotID)
+        {
+            
+        }
+
         public static List<CampingSpot> GetCampingSpotList()
         {
             return null;
