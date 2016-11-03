347d61be336296443f9f4ec4bf692a359217686b
IJpelaar,Bono B.G.M. <b.ijpelaar@student.fontys.nl>
2016-11-03 15:01:45 +0100

    Deel van de lege dataklassen toegevoegd, to be continued...

---
+++ EyeCT4Events/Data/DataClasses/DataMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeCT4Events.Data.DataClasses
+{
+    class DataMessage
+    {
+        public DataMessage()
+        {
+            
+        }
+
+        public static Message GetMessage()
+        {
+            return null;
+        }
+
+        public static void SetMessage()
+        {
+            
+        }
+
+        public static void UpdateMessage()
+        {
+            
+        }
+
+        public static List<Message> GetMessageList()
+        {
+            return null;
+        }
+    }
+}
