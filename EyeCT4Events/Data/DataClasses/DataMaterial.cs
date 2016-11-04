0b4a40b968700fffe8bb8fb942752607febfc492
IJpelaar,Bono B.G.M. <b.ijpelaar@student.fontys.nl>
2016-11-04 09:21:17 +0100

    available material lijst op kunnen halen

--- EyeCT4Events/Data/DataClasses/DataMaterial.cs
+++ EyeCT4Events/Data/DataClasses/DataMaterial.cs
@@ -21,14 +21,44 @@ namespace EyeCT4Events.Data.DataClasses
                                             $"FROM Product " +
                                             $"WHERE ProductID = {materialID}",
                                             Datacom.connect);
-
             SqlDataReader reader = cmd.ExecuteReader();
             reader.Read();
             string name = reader.GetString(0);
             decimal price = reader.GetDecimal(1);
+            reader.Close();
+            Datacom.CloseConnection();
 
             Material m = new Material(name, MaterialType.USB, price, false);
             return m;
+        }
+
+        public static List<String> AvailableMaterialList()
+        {
+            Datacom.OpenConnection();
+            SqlCommand cmd = new SqlCommand("SELECT DISTINCT Omschrijving, Naam, PrijsPerDag" +
+                                            "FROM Product" +
+                                            "WHERE PStatus = Beschikbaar");
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<string> producten = new List<string>();
+            while (reader.Read())
+            {
+                string omschrijving = reader.GetString(0);
+                string naam = reader.GetString(1);
+                decimal prijs = reader.GetDecimal(2);
+
+                string product = $"{naam} - {omschrijving} - {prijs}";
+
+                producten.Add(product);
+            }
+            reader.Close();
+            Datacom.CloseConnection();
+
+            return producten;
+        }
+
+        public static Material SetMaterialHired()
+        {
+            return null;
         }
 
         public static void UpdateMaterial()
