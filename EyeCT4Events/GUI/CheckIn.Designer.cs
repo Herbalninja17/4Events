6d2ff3668ebb2521cfa89221fe601e0a39922e11
Rechardmotieram17 <Rechardmotieram17@hotmail.com>
2016-11-04 08:58:57 +0100

    yo

---
+++ EyeCT4Events/GUI/CheckIn.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace EyeCT4Events.GUI
+{
+    partial class CheckIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Place Wristband On The Scanner";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // CheckIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Name = "CheckIn";
+            this.Text = "CheckIn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ EyeCT4Events/GUI/CheckIn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EyeCT4Events.GUI;
+
+namespace EyeCT4Events.GUI
+{
+    public partial class CheckIn : Form
+    {
+        public CheckIn()
+        {
+            InitializeComponent();
+        }
+
+        RfidForm RFID = new RfidForm();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (RFID.ShowDialog() == DialogResult.OK) //wait for OK 
+            {
+                MessageBox.Show("Welcome: " + RFID.tagstring.ToString());
+            }
+        }
+    }
+}
