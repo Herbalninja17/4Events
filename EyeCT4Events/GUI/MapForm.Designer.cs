0f5ff468cfb9325a96bbf707ea1f485705976476
IJpelaar,Bono B.G.M. <b.ijpelaar@student.fontys.nl>
2016-11-04 14:05:22 +0100

    piep

--- EyeCT4Events/GUI/MapForm.Designer.cs
+++ EyeCT4Events/GUI/MapForm.Designer.cs
@@ -30,15 +30,19 @@
         {
             this.btnMapChooseLocation = new System.Windows.Forms.Button();
             this.pbMap = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbMap)).BeginInit();
             this.SuspendLayout();
             // 
             // btnMapChooseLocation
             // 
-            this.btnMapChooseLocation.Location = new System.Drawing.Point(1472, 23);
-            this.btnMapChooseLocation.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnMapChooseLocation.Location = new System.Drawing.Point(812, 23);
+            this.btnMapChooseLocation.Margin = new System.Windows.Forms.Padding(6);
             this.btnMapChooseLocation.Name = "btnMapChooseLocation";
-            this.btnMapChooseLocation.Size = new System.Drawing.Size(236, 79);
+            this.btnMapChooseLocation.Size = new System.Drawing.Size(180, 79);
             this.btnMapChooseLocation.TabIndex = 0;
             this.btnMapChooseLocation.Text = "Kies Locatie";
             this.btnMapChooseLocation.UseVisualStyleBackColor = true;
@@ -46,25 +50,66 @@
             // 
             // pbMap
             // 
-            this.pbMap.Location = new System.Drawing.Point(24, 173);
-            this.pbMap.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pbMap.Image = global::EyeCT4Events.Properties.Resources.Camping_ReeënDal;
+            this.pbMap.Location = new System.Drawing.Point(24, 114);
+            this.pbMap.Margin = new System.Windows.Forms.Padding(6);
             this.pbMap.Name = "pbMap";
-            this.pbMap.Size = new System.Drawing.Size(1684, 733);
+            this.pbMap.Size = new System.Drawing.Size(981, 875);
+            this.pbMap.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbMap.TabIndex = 1;
             this.pbMap.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(133, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(361, 33);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(619, 47);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(152, 33);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Type:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(515, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nummer:";
             // 
             // MapForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1732, 929);
+            this.ClientSize = new System.Drawing.Size(1042, 1004);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.pbMap);
             this.Controls.Add(this.btnMapChooseLocation);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "MapForm";
             this.Text = "Kaart";
             ((System.ComponentModel.ISupportInitialize)(this.pbMap)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,5 +117,9 @@
 
         private System.Windows.Forms.Button btnMapChooseLocation;
         private System.Windows.Forms.PictureBox pbMap;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
